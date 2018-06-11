a7446dcd4fcf9fa7d8e8d00c1d9ab315b55c53bc
Max Scheurer <max.scheurer@me.com>
2018-06-11 14:01:28 +0200

    build Hamiltonian blocks

--- pymeet/FrenkelHamiltonian.py
+++ pymeet/FrenkelHamiltonian.py
@@ -32,11 +32,24 @@ class FrenkelHamiltonian:
 
     def __init__(self, chromophores):
         self.chromophores = chromophores
+        self.matrix = None
+        self.evals, self.evecs = None, None
 
     def construct(self):
         # get possible combinations of chromophores
         chromophore_combinations = \
             list(itertools.combinations(range(len(self.chromophores)), 2))
+
+        n = 0
+        n_states_chromophore_start = []
+        all_exc_energies = np.array([])
+        for c in self.chromophores:
+            n_states_chromophore_start.append(n)
+            n += c.n_states
+            all_exc_energies = np.concatenate((all_exc_energies, c.etenergies))
+        self.matrix = np.zeros(shape=(n, n))
+        np.fill_diagonal(self.matrix, all_exc_energies)
+        print("Shape of H: ", self.matrix.shape)
 
         for combination in chromophore_combinations:
             c1, c2 = self.chromophores[combination[0]], self.chromophores[combination[1]]
@@ -67,7 +80,14 @@ class FrenkelHamiltonian:
                                                                 -r12)
         #                         print(en)
             assert np.allclose(J_1, J_1_test) is True
-            print(J_1)
+            i = n_states_chromophore_start[combination[0]]
+            j = n_states_chromophore_start[combination[1]]
+            coupling_block = J_0 + J_1
+            self.matrix[i:i + c1.n_states,
+                        j:j + c2.n_states] = coupling_block
+            self.matrix[j:j + c2.n_states,
+                        i:i + c1.n_states] = coupling_block.transpose(1, 0)
+        print(self.matrix)
 
     def diagonalize(self):
-        pass
+        self.evals, self.evecs = np.linalg.eigh(self.matrix)
